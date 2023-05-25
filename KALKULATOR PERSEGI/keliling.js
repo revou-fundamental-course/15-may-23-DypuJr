d72b9e6f8af08b3a5c637847a71213f1599f7503
DypuJr <133724329+DypuJr@users.noreply.github.com>
2023-05-25 16:26:56 +0700

    Delete KALKULATOR PERSEGI directory

--- KALKULATOR PERSEGI/keliling.js
+++
@@ -1,6 +0,0 @@
-function hasilKeliling(){
-    let a = document.getElementById("nilaiSisi").value 
-    let b = 4
-
-    document.getElementById("demo").innerHTML = a * b;
-}
--- KALKULATOR PERSEGI/luas.js
+++
@@ -1,5 +0,0 @@
-function hasilLuas(){
-    let hasil = document.getElementById("nilaiSisi").value 
-
-    document.getElementById("demo").innerHTML = hasil * hasil;
-}
