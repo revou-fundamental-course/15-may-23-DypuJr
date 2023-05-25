f37ea4c8b0c42acd14f1c44bc3b8b4bf605939f3
DypuJr <133724329+DypuJr@users.noreply.github.com>
2023-05-25 17:44:19 +0700

    initialize files

---
+++ fungsi.js
@@ -0,0 +1,12 @@
+function hasilKeliling(){
+    let a = document.getElementById("nilaiSisi1").value 
+    let b = 4
+
+    document.getElementById("demo1").innerHTML = a * b;
+}
+
+function hasilLuas(){
+    let hasil = document.getElementById("nilaiSisi").value 
+
+    document.getElementById("demo").innerHTML = hasil * hasil;
+}
